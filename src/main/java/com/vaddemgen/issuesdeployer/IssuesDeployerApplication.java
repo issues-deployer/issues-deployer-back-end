3a221fbc01cd577775ad0654b6c5274fc1811599
Vadym Demchuk <vadym.demchuk.bitmedia@gmail.com>
2019-05-19 20:09:04 +0300

    Create initial spring project.

---
+++ src/main/java/com/vaddemgen/issuesdeployer/IssuesDeployerApplication.java
@@ -0,0 +1,13 @@
+package com.vaddemgen.issuesdeployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesDeployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesDeployerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaddemgen/issuesdeployer/IssuesDeployerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vaddemgen.issuesdeployer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssuesDeployerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
